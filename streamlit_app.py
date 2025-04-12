4e68a01045d79491f50bdfa824c2c7e049b631b0
Alessandra Batalha <98042505+alebatalha@users.noreply.github.com>
2025-04-12 14:10:07 +0100

    renaime problem solve

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ from backend.task_breakdown import break_task
 from backend.summarization import summarize_content
 from backend.question_generation import generate_quiz
 from backend.chat_assistant import ask_question
+import os
+import sys
 
 # App metadata
 APP_TITLE = "TaskTamer"
